6f4d78ebf1ad75d183791a3edc049e6c0a8a1644
Love Beling <love.beling@se.experis.com>
2022-03-03 17:00:41 +0100

    Initialized project

---
+++ src/main/java/com/example/mandelbrotserver/MandelbrotServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mandelbrotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandelbrotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandelbrotServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mandelbrotserver/MandelbrotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mandelbrotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandelbrotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
